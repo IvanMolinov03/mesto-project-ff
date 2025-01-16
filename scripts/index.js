3d7943411f3724f0e208412f67ed8d4b650a2aeb
Ivan Molinov <ivan.molinov@mail.ru>
2025-01-16 20:35:39 +0300

    added creationCard and deleteCard functions

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+
+function createCard (imgSource, imgName, removeCard)  {
+    
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+        
+    cardElement.querySelector('.card__image').src = imgSource;
+    cardElement.querySelector('.card__title').textContent = imgName;
+    deleteButton.addEventListener('click', () => removeCard(deleteButton));
+    return cardElement;
+}
+
+function deleteCard(itemToDel) {
+    itemToDel = itemToDel.closest('.card');
+    itemToDel.remove();
+}
+     
+initialCards.forEach(function(item) {
+    cardList.append(createCard(item.link, item.name, deleteCard));
+})
