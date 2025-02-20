31758be6234bd64e2c4efea13020247fc35db585
Иван Молинов <ivanmolinov@MacBook-Pro-Ivan.local>
2025-02-20 10:06:44 +0300

    save before module work

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,20 @@
 export function createCard (imgSource, imgName, removeCard, likeCard, openCard)  {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.cloneNode(true);
+    const cardElement = document.querySelector('#card-template').content.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
     const likeButton = cardElement.querySelector('.card__like-button');
-        
-    cardElement.querySelector('.card__image').src = imgSource;
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardImage.src = imgSource;
+    cardImage.alt = imgSource;
     cardElement.querySelector('.card__title').textContent = imgName;
-    deleteButton.addEventListener('click', () => removeCard(deleteButton));
+    deleteButton.addEventListener('click', () => removeCard(cardElement));
     likeButton.addEventListener('click', () => likeCard(likeButton));
-    cardElement.querySelector('.card__image').addEventListener('click', () => openCard(imgSource, imgName));
+    cardImage.addEventListener('click', () => openCard(imgSource, imgName));
     return cardElement;
 }
 
-export function deleteCard(itemToDel) {
-    itemToDel = itemToDel.closest('.card');
-    itemToDel.remove();
+export function deleteCard(card) {
+    card.remove();
 }
 
 export function likeCard(listener) {
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonAdd = document.querySelector('.profile__add-button');
 const profileForm = document.forms.editProfile;
 const placeForm = document.forms.newPlace;
+const placeName = document.forms.newPlace.placeName;
+const placeLink = document.forms.newPlace.link;
 
 initialCards.forEach(function(card) {
     cardList.append(createCard(card.link, card.name, deleteCard, likeCard, openCard));
@@ -17,8 +19,6 @@ initialCards.forEach(function(card) {
 
 function addUserCard(evt) {
     evt.preventDefault();
-    const placeName = document.forms.newPlace.placeName;
-    const placeLink = document.forms.newPlace.link;
     cardList.prepend(createCard(placeLink.value, placeName.value, deleteCard, likeCard, openCard));
     placeForm.reset();
     closePopup();
